a6f0c2886a40f8f9697e66b70a6fda230cf71d13
NToan324 <nhattoan664t@gmail.com>
2025-03-03 00:55:18 +0700

    resolve conflict

---
+++ lib/views/pages/home/widgets/avatar_widget.dart
@@ -0,0 +1,45 @@
+import 'package:computer_sales_app/config/color.dart';
+import 'package:computer_sales_app/utils/responsive.dart';
+import 'package:flutter/material.dart';
+
+class AvatarWidget extends StatelessWidget {
+  const AvatarWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            spacing: 10,
+            children: [
+              Container(
+                width: Responsive.isDesktop(context) ? 50 : 40,
+                height: Responsive.isDesktop(context) ? 50 : 40,
+                decoration: BoxDecoration(
+                    color: AppColor.primary, shape: BoxShape.circle),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.notifications_rounded,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              CircleAvatar(
+                backgroundImage: AssetImage(
+                  'assets/images/avatar.jpeg',
+                ),
+                radius: Responsive.isDesktop(context) ? 25 : 20,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
