571aa9e587cc82531da3d0d90d6b20d762be25d9
NToan324 <nhattoan664t@gmail.com>
2025-03-05 12:22:51 +0700

    complete home

--- lib/views/pages/product_details/product_details_view.dart
+++ lib/views/pages/product_details/product_details_view.dart
@@ -5,6 +5,13 @@ class ProductDetailsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Product Details'),
+      ),
+      body: Center(
+        child: Text('Product Details'),
+      ),
+    );
   }
 }
