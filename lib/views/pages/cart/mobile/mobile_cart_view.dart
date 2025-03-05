fe53b8f66051926d974708a8c6e3650b79d9bf43
StevePenguin <dogiahuy2252004@gmail.com>
2025-03-05 14:47:58 +0700

    synchronize color, font

--- lib/views/pages/cart/mobile/mobile_cart_view.dart
+++ lib/views/pages/cart/mobile/mobile_cart_view.dart
@@ -1,5 +1,6 @@
 import 'package:computer_sales_app/config/color.dart';
 import 'package:computer_sales_app/config/font.dart';
+import 'package:computer_sales_app/views/pages/cart/mobile/widget/SummaryWidget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:computer_sales_app/utils/widget/CustomAppBar.dart';
@@ -198,74 +199,13 @@ class _MobileCartViewState extends State<MobileCartView> {
                   ),
                 ],
               ),
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Container(
-                  padding: const EdgeInsets.all(16.0),
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(10),
-                      topRight: Radius.circular(10),
-                    ),
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        color: AppColor.black,
-                        offset: Offset(0, 5),
-                        blurRadius: 20.0,
-                      ),
-                    ],
-                  ),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        height: 60,
-                        child: TextField(
-                          textAlign: TextAlign.start,
-                          controller: _promoCodeController,
-                          decoration: InputDecoration(
-                            hintText: 'Promo Code',
-                            focusedBorder: const OutlineInputBorder(
-                              borderSide: BorderSide(color: AppColor.black),
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(12.0)),
-                            ),
-                            border: const OutlineInputBorder(
-                              borderSide: BorderSide(color: AppColor.black),
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(12.0)),
-                            ),
-                            suffixIcon: Padding(
-                                padding: const EdgeInsets.all(10),
-                                child: Button(
-                                    text: "Apply", onPressed: _applyPromoCode)),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      _buildSummaryRow('Subtotal', subtotal),
-                      _buildSummaryRow('Delivery Fee', deliveryFee),
-                      _buildSummaryRow('Discount', -discount),
-                      const Divider(),
-                      _buildSummaryRow('Total', total, isTotal: true),
-                      const SizedBox(height: 10),
-                      SizedBox(
-                        width: double.infinity,
-                        height: 50,
-                        child: Button(
-                          onPressed: () {
-                            // Checkout
-                          },
-                          text: 'Proceed to checkout',
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+              SummaryWidget(
+                promoCodeController: _promoCodeController,
+                applyPromoCode: _applyPromoCode,
+                subtotal: subtotal,
+                deliveryFee: deliveryFee,
+                discount: discount,
+                total: total,
               ),
             ],
           ),
@@ -356,31 +296,6 @@ class _MobileCartViewState extends State<MobileCartView> {
             ),
           ),
       ],
-    );
-  }
-
-  Widget _buildSummaryRow(String label, double amount, {bool isTotal = false}) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 4.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            label,
-            style: TextStyle(
-              fontSize: FontSizes.medium,
-              fontWeight: isTotal ? FontWeight.bold : FontWeight.normal,
-            ),
-          ),
-          Text(
-            '\$${amount.toStringAsFixed(2)}',
-            style: TextStyle(
-              fontSize: FontSizes.medium,
-              fontWeight: isTotal ? FontWeight.bold : FontWeight.normal,
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
